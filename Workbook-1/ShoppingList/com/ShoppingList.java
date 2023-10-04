d1bfa5db0d58a152c2c25b187708f6dcd936584d
dita-b <aninditabarman1800@gmail.com>
2023-10-04 11:10:09 -0400

    Finished the shopping list exercise

---
+++ Workbook-1/ShoppingList/com/ShoppingList.java
@@ -0,0 +1,10 @@
+package com;
+
+public class ShoppingList {
+    public static void main{
+        System.out.println("Eggs, Milk, Bread, Fruits, Kombucha, Yougurt, Chicken thighs");
+    }
+}
+git add -A
+git commit -m "Completed shoppinglist app"
+git push origin main 
